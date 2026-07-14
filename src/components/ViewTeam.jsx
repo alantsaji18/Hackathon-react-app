710e9f8f5b75820eab24013a86fb7324d772898e
alantsaji18 <alantsaji10@gmail.com>
2026-07-14 22:53:04 +0530

    Project updates

--- src/components/ViewTeam.jsx
+++ src/components/ViewTeam.jsx
@@ -1,83 +1,81 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-
-
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Nav from "./Nav";
 
 const ViewTeam = () => {
-    const [data,changeData] = useState([])
-    const fetchData =()=>{
-        axios.get("http://localhost:3000/view-data").then(
-            (response) => {
-                changeData(response.data)
-            }
-        ).catch()
-    }
-    useEffect(
-        () => {
-            fetchData()
-        },[]
-    )
-  return (
-    <div>
+  const [data, setData] = useState([]);
 
-            <div className="container mt-4">
-        <h2 className="text-center mb-4">
-          <b>View Team</b>
-        </h2>
-        <table className="table">
-          <thead>
+  const fetchData = () => {
+    axios
+      .get("http://localhost:3000/view-data")
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container-fluid">
+      <Nav />
+
+      <h2 className="text-center mt-3 mb-3">Team Details</h2>
+
+      <div className="table-responsive">
+        <table className="table table-bordered table-striped table-hover w-100">
+          <thead className="table-dark text-center">
             <tr>
-              <th scope="col">Team ID</th>
-              <th scope="col">Team Name</th>
-              <th scope="col">Team Leader Name</th>
-              <th scope="col">Team Leader Email</th>
-              <th scope="col">Team Leader Phone</th>
-              <th scope="col">College Name</th>
-              <th scope="col">N.O Members</th>
-              <th scope="col">Project Title</th>
-              <th scope="col">Problem Statement</th>
-              <th scope="col">Technology Stack</th>
-              <th scope="col">Mentor Name</th>
-              <th scope="col">Registration Date</th>
-              <th scope="col">Table Number</th>
+              <th>Team ID</th>
+              <th>Team Name</th>
+              <th>Leader Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>College</th>
+              <th>No. Members</th>
+              <th>Project Title</th>
+              <th>Problem Statement</th>
+              <th>Tech Stack</th>
+              <th>Mentor</th>
+              <th>Reg Date</th>
+              <th>Table No</th>
             </tr>
           </thead>
-          
-                <tbody>
-                  {
-                    data.map
-                    (
-                        (value,index)=>{
-                            return(
-                                <tr>
-                                    <td>{value.Teamid}</td>
-                                    <td>{value.Teamname}</td>
-                                    <td>{value.Teamleadername}</td>
-                                    <td>{value.Leaderemail}</td>
-                                    <td>{value.LeaderPhone}</td>
-                                    <td>{value.Collegename}</td>
-                                    <td>{value.Nomembers}</td>
-                                    <td>{value.Projecttitle}</td>
-                                    <td>{value.Problemstatement}</td>
-                                    <td>{value.Technologystack}</td>
-                                    <td>{value.Mentorname}</td>
-                                    <td>{value.Regdate}</td>
-                                    <td>{value.Tableno}</td>
-                                </tr>
-                            )
-                        }
 
-                    )}
+          <tbody className="text-center">
+            {data.length > 0 ? (
+              data.map((value, index) => (
+                <tr key={index}>
+                  <td>{value.teamid}</td>
+                  <td>{value.teamname}</td>
+                  <td>{value.teamleadername}</td>
+                  <td>{value.leaderemail}</td>
+                  <td>{value.leaderphone}</td>
+                  <td>{value.collegename}</td>
+                  <td>{value.nomembers}</td>
+                  <td>{value.projecttitle}</td>
+                  <td>{value.problemstatement}</td>
+                  <td>{value.technologystack}</td>
+                  <td>{value.mentorname}</td>
+                  <td>{value.regdate}</td>
+                  <td>{value.tableno}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="13">No Data Found</td>
+              </tr>
+            )}
           </tbody>
-             
-          
         </table>
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
 export default ViewTeam;
